47e971e7f94923e4fe0c98be681dbe9dce7e81c1
sumithanvat <skhanvat@gmail.com>
2023-05-08 01:28:07 +0530

    Added:Valid mobile password

--- src/com/bridgelabz/ValidMobilePassword.java
+++ src/com/bridgelabz/ValidMobilePassword.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ValidMobileNumber {
+public class ValidMobilePassword {
 
     public static void main(String[] args) {
         // taking user input
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Mobile number ");
-        String mobileNumber = input.next();
-        Pattern pattern4 = Pattern.compile("^[0-9]{2,}[0-9]{10,}$");
-        Matcher matcher4 = pattern4.matcher(mobileNumber);
+        System.out.println("Enter Mobile Password ");
+        String mobilePassword = input.next();
+        Pattern pattern4 = Pattern.compile("^[a-z]{8,}$");
+        Matcher matcher4 = pattern4.matcher(mobilePassword);
         if (matcher4.matches()) {
             System.out.println("valid");
         } else {
