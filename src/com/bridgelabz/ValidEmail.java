f3c53a196e18c2f13356775e4f2782c6155ff54e
sumithanvat <skhanvat@gmail.com>
2023-05-08 01:06:04 +0530

    Added:Valid Email

--- src/com/bridgelabz/ValidEmail.java
+++ src/com/bridgelabz/ValidEmail.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ValidLatName {
+public class ValidEmail {
 
     public static void main(String[] args) {
         // taking user input
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Last name");
+        System.out.println("Enter Email");
 
-        // storing input to first name
-        String lastName = input.nextLine();
+        // storing input to Email
+        String Email = input.nextLine();
 
         // Regex pattern to match first name
-        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+        Pattern pattern = Pattern.compile("^[0-9a-zA-Z]+([a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$");
 
         // Match the pattern against the input
-        Matcher matcher = pattern.matcher(lastName);
+        Matcher matcher = pattern.matcher(Email);
 
         // Check if the input matches the pattern
         if (matcher.matches()) {
 
-            System.out.println("Last name is valed");
+            System.out.println("Email is valed");
 
         } else {
-            System.out.println("invalid Last name");
+            System.out.println("invalid Email");
         }
     }
 }
